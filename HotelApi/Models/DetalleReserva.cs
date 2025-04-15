decc70f89da999c3577c759feb267b84dcee56b4
manuayalag <cesarayala@fiuni.edu.py>
2025-04-15 12:02:45 -0300

    Agregue modelos reserva y pension

---
+++ HotelApi/Models/DetalleReserva.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace HotelApi.Models
+{
+    public class DetalleReserva
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }  // corresponde a id_detalle_reserva
+
+        [Required]
+        public int ReservaId { get; set; }
+        public Reserva Reserva { get; set; }
+
+        public int? HabitacionId { get; set; }  // nullable como en tu DB
+        public Habitacion Habitacion { get; set; }
+
+        [Required]
+        [Range(0, 99)]
+        public int CantidadAdultos { get; set; }
+
+        [Required]
+        [Range(0, 99)]
+        public int CantidadNinhos { get; set; }
+
+        [Required]
+        public int PensionId { get; set; }  // suponiendo que tienes una tabla de pensiones
+        public Pension Pension { get; set; }  // propiedad de navegación
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Creacion { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Actualizacion { get; set; }
+
+        [Required]
+        public bool Activo { get; set; } = true;
+    }
+}
---
+++ HotelApi/Models/Pension.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApi.Models
+{
+    public class Pension
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }  // id_pension
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [StringLength(250)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal PrecioAdicional { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Creacion { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Actualizacion { get; set; }
+
+        [Required]
+        public bool Activo { get; set; } = true;
+    }
+}
