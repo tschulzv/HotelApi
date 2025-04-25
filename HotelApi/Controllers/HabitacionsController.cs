4a73607cce7aa0116f96e6c864b11596178590ad
manuayalag <cesarayala@fiuni.edu.py>
2025-04-25 14:22:16 -0300

    agregue DbContext

---
+++ HotelApi/Controllers/HabitacionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelApi.Data;
+using HotelApi.Models;
+
+namespace HotelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HabitacionsController : ControllerBase
+    {
+        private readonly HotelApiContext _context;
+
+        public HabitacionsController(HotelApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Habitacions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Habitacion>>> GetHabitacion()
+        {
+            return await _context.Habitacion.ToListAsync();
+        }
+
+        // GET: api/Habitacions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Habitacion>> GetHabitacion(int id)
+        {
+            var habitacion = await _context.Habitacion.FindAsync(id);
+
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+
+            return habitacion;
+        }
+
+        // PUT: api/Habitacions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHabitacion(int id, Habitacion habitacion)
+        {
+            if (id != habitacion.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(habitacion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HabitacionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Habitacions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Habitacion>> PostHabitacion(Habitacion habitacion)
+        {
+            _context.Habitacion.Add(habitacion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHabitacion", new { id = habitacion.Id }, habitacion);
+        }
+
+        // DELETE: api/Habitacions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHabitacion(int id)
+        {
+            var habitacion = await _context.Habitacion.FindAsync(id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Habitacion.Remove(habitacion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HabitacionExists(int id)
+        {
+            return _context.Habitacion.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HotelApi/Data/HotelApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HotelApi.Models;
+
+namespace HotelApi.Data
+{
+    public class HotelApiContext : DbContext
+    {
+        public HotelApiContext (DbContextOptions<HotelApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HotelApi.Models.Cliente> Cliente { get; set; } = default!;
+        public DbSet<HotelApi.Models.Habitacion> Habitacion { get; set; } = default!;
+    }
+}
