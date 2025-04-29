6563c5e460b3ceb4ea17780851fc5bdcb59be47f
manuayalag <cesarayala@fiuni.edu.py>
2025-04-29 09:09:31 -0300

    elimine tabla CancelacionDTO de la DB y migre

--- HotelApi/Data/HotelApiContext.cs
+++ HotelApi/Data/HotelApiContext.cs
@@ -26,7 +26,6 @@ namespace HotelApi.Data
         public DbSet<HotelApi.Models.DetalleHuesped> DetalleHuesped { get; set; } = default!;
         public DbSet<HotelApi.Models.DetalleReserva> DetalleReserva { get; set; } = default!;
         public DbSet<HotelApi.Models.EstadoReserva> EstadoReserva { get; set; } = default!;
-        public DbSet<HotelApi.Models.CancelacionDTO> CancelacionDTO { get; set; } = default!;
         public DbSet<HotelApi.Models.Servicio> Servicio { get; set; } = default!;
         public DbSet<HotelApi.Models.Solicitud> Solicitud { get; set; } = default!;
         public DbSet<HotelApi.Models.TipoDocumento> TipoDocumento { get; set; } = default!;
