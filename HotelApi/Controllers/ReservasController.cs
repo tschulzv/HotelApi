7ff2ac4f8719ef658cbbb07323a1ba83314e1126
manuayalag <cesarayala@fiuni.edu.py>
2025-05-02 16:03:02 -0300

    Modifique ReservasController para que traiga tambien los detalles en GET

--- HotelApi/Controllers/ReservasController.cs
+++ HotelApi/Controllers/ReservasController.cs
@@ -26,7 +26,11 @@ namespace HotelApi.Controllers
         public async Task<ActionResult<IEnumerable<ReservaDTO>>> GetReservas()
         {
             // obtener solo los activos
-            var res = await _context.Reserva.Where(r => r.Activo).ToListAsync();
+            var res = await _context.Reserva
+            .Where(r => r.Activo)
+            .Include(r => r.Detalles)
+            .ToListAsync();
+
             var resDtos = res.Select(r => ToDTO(r));
             return Ok(resDtos);
         }
@@ -35,7 +39,10 @@ namespace HotelApi.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<ReservaDTO>> GetReserva(int id)
         {
-            var reserva = await _context.Reserva.Where(r => r.Activo && r.Id == id).FirstOrDefaultAsync();
+            var reserva = await _context.Reserva
+            .Include(r => r.Detalles) // 👈 Agregá esto también
+            .Where(r => r.Activo && r.Id == id)
+            .FirstOrDefaultAsync();
 
             if (reserva == null)
             {
