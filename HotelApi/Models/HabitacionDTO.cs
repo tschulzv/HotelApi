caecccae0362ec485f72e7781a58e06d95b40013
manuayalag <cesarayala@fiuni.edu.py>
2025-05-06 16:49:26 -0300

    cambie rooms y agregue columnas y eso

--- HotelApi/Models/HabitacionDTO.cs
+++ HotelApi/Models/HabitacionDTO.cs
@@ -3,9 +3,12 @@
     public class HabitacionDTO
     {
         public int Id { get; set; }
-        public int TipoHabitacionId { get; set; }
         public int NumeroHabitacion { get; set; }
+        public int TipoHabitacionId { get; set; }
+        public string TipoHabitacionNombre { get; set; } = string.Empty;
         public int EstadoHabitacionId { get; set; }
+        public string EstadoNombre { get; set; } = string.Empty;
+        public string Observaciones { get; set; }
         public bool Activo { get; set; }
     }
 }
